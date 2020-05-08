268efebd9a3df6aa23146f9f46050663dfa71e64
Ting Chung <tchung682@gmail.com>
2020-05-08 19:32:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,8 +29,8 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
 
-var isAge =  (age>=13 && age<=19) ?   "You are a teenager" :  "You are not a teenager"
-console.log(isAge)
+return  (age>=13 && age<=19) ?   "You are a teenager" :  "You are not a teenager"
+
 }
 
 function switchAge(age) {
